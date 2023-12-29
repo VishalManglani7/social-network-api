8b3bf9d8f478003362c5c71f89cd8e21c516d9c5
Vishal <vishal.manglani7@gmail.com>
2023-12-28 21:04:31 -0500

    continured to build out and completed controllers

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -10,5 +10,5 @@ const {
 
 router.route("/").get(getThoughts).post(createThought);
 
-router.route("/:thoughtID").get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete(deleteThought);
 module.exports = router
